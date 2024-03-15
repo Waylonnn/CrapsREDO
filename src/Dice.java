110c58146c78ef4c148e9473f2148da4efa961f9
Waylon! <weese.619547@swl.k12.oh.us>
2024-03-15 01:24:25 -0400

    Initital

--- src/Dice.java
+++ src/Dice.java
@@ -2,14 +2,14 @@ public class Dice {
     int max = 6;
     int min = 1;
     int range = max - min + 1;
-    int roll = (int)(Math.random() * range) + min;
+
 
     private int num = 0;
     public Dice(){
         roll();
     }
     public void roll(){
-        num = roll;
+        num = (int)(Math.random() * range) + min;
     }
     public int getRoll(){
         return num;
